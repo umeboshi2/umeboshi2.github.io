2f97b7262029c317c52c2b5c5a292b7595b80f2b
Joseph Rawson <joseph.rawson.works@littledebian.org>
2017-02-11 14:33:21 -0600

    added new sass and compass config

--- config.rb
+++ config.rb
@@ -6,14 +6,16 @@ require 'compass-ui'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "assets/stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+fonts_dir = "assets/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+#output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
